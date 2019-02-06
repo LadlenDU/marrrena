03c8b43cf5fabaaec5ad30b5adc0f0ddcea0e5fc
Ladlen <TwilightTower@mail.ru>
2019-02-06 19:29:46 +0300

    copy sheet into separate file

--- split.product.import.xlsx.php
+++ split.product.import.xlsx.php
@@ -10,6 +10,8 @@ ini_set('display_errors', 1);
 set_time_limit(0);
 ini_set('memory_limit', '990M');
 
+require_once 'ProductExcellGenerator.class.php';
+
 require 'vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
@@ -35,26 +37,43 @@ class Splitter
     public $origSpreadsheet;
     public $splittingDirName;
 
+    const MAX_ROWS = 10;
+
     public function __construct($sourceDir, $fName)
     {
         $this->origSpreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($sourceDir . '/' . $fName);
 
         $dirName = explode('.', $fName)[0];
         $this->splittingDirName = $sourceDir . '/' . $dirName;
-        if (is_dir($this->splittingDirName)) {
+        /*if (is_dir($this->splittingDirName)) {
             throw new \Exception('Директория уже существует: ' . $dirName);
-        }
-        mkdir($this->splittingDirName);
+        }*/
+        @mkdir($this->splittingDirName);
     }
 
     public function splitSheet($sheetName)
     {
         $sheet = $this->origSpreadsheet->getSheetByName($sheetName);
 
-        $headerRow = $sheet->getHighestRow();
-
         $excellGenerator = new ProductExcellGenerator($sheetName);
-        //$excellGenerator->addSheetRow($name, $data);
-        $excellGenerator->saveToFile($this->splittingDirName . '/' . $sheetName);
+
+        $rowIterator = $sheet->getRowIterator();
+        foreach ($rowIterator as $keyRow => $row) {
+
+            $rowData = [];
+
+            $cellIterator = $row->getCellIterator();
+            foreach ($cellIterator as $keyCell => $cell) {
+                $rowData[] = $cell->getValue();
+            }
+
+            if ($keyRow == 1) {
+                $excellGenerator->setSheetHeader($sheetName, $rowData);
+            } else {
+                $excellGenerator->addSheetRow($sheetName, $rowData);
+            }
+        }
+
+        $excellGenerator->saveToFile($this->splittingDirName . '/' . $sheetName . '.xlsx');
     }
 }

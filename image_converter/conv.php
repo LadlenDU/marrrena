3b4767a3277c63da75927dd2b1d9a0882dec502e
Ladlen <TwilightTower@mail.ru>
2019-03-07 15:53:01 +0300

    image conversion stuff

--- image_converter/conv.php
+++ image_converter/conv.php
@@ -22,15 +22,27 @@ $position = "LT";
 
 $layer->cropInPixel($newWidth, $newHeight, $positionX, $positionY, $position);
 
-$layerLogo = ImageWorkshop::initFromPath($imageDir . '/104829-970-jpg.jpg');
+$layerLogo = ImageWorkshop::initFromPath(__DIR__ . '/logo.png');
+if ($newWidth < $newHeight) {
+    $layerLogo->resizeInPixel($newWidth / 2, null, false);
+} else {
+    $layerLogo->resizeInPixel(null, $newHeight / 2, false);
+}
+
+$emptyLayer = ImageWorkshop::initVirginLayer($newWidth, $newHeight);
+$emptyLayer->addLayerOnTop($layer, 0, 0, 'LT');
+$emptyLayer->addLayerOnTop($layerLogo, 0, 0, 'MM');
 
 ////
 $dirPath = $imageDir;
-$filename = "104829-970-jpg.jpg";
+$filename = "104829-970-jpg.mod.jpg";
 $createFolders = true;
 $backgroundColor = null;    // transparent, only for PNG (otherwise it will be white if set null)
 $imageQuality = 90;         // useless for GIF, usefull for PNG and JPEG (0 to 100%)
 
-$layer->save($dirPath, $filename, $createFolders, $backgroundColor, $imageQuality);
+$emptyLayer->save($dirPath, $filename, $createFolders, $backgroundColor, $imageQuality);
+
+file_put_contents('conv.log', $filename . "\n", FILE_APPEND);
+echo $filename . "\n";
 
 echo 'DONE';

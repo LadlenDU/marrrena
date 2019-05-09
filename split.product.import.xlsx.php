7dd093932b359de14447ba0df2bff82ba96ee8eb
Ladlen <TwilightTower@mail.ru>
2019-05-09 07:44:29 +0300

    split product stuff

--- split.product.import.xlsx.php
+++ split.product.import.xlsx.php
@@ -22,6 +22,9 @@ foreach ($dir as $fileinfo) {
         if ($fileinfo->isFile()) {
             $splitter = new Splitter($sourceDir, $fileinfo->getFilename());
             $splitter->splitSheet('Products');
+            $splitter->splitSheet('AdditionalImages');
+            $splitter->splitSheet('ProductAttributes');
+            $splitter->splitSheet('ProductSEOKeywords');
         }
     }
 }
@@ -31,7 +34,7 @@ class Splitter
     public $origSpreadsheet;
     public $splittingDirName;
 
-    const MAX_ROWS = 10;
+    const MAX_ROWS = 20;
 
     public function __construct($sourceDir, $fName)
     {
@@ -39,22 +42,18 @@ class Splitter
 
         $dirName = explode('.', $fName)[0];
         $this->splittingDirName = $sourceDir . '/' . $dirName;
-        /*if (is_dir($this->splittingDirName)) {
+        if (is_dir($this->splittingDirName)) {
             throw new \Exception('Директория уже существует: ' . $dirName);
-        }*/
-        @mkdir($this->splittingDirName);
+        }
+        mkdir($this->splittingDirName);
     }
 
     public function splitSheet($sheetName)
     {
         $sheet = $this->origSpreadsheet->getSheetByName($sheetName);
 
-        #$excellGenerator = false;
-
         $rowCounter = 0;
         $pageCounter = 0;
-
-        #$hasUnsavedRows = false;
 
         $rowHeaderData = [];
 
@@ -77,28 +76,6 @@ class Splitter
 
             $pages[$pageCounter][] = $rowData;
 
-            /*if (!$rowCounter) {
-                if ($excellGenerator) {
-                    $excellGenerator->saveToFile($this->splittingDirName . '/' . $sheetName . "_{$pageCounter}.xlsx");
-                    unset($excellGenerator);
-                    gc_collect_cycles();
-                    ++$pageCounter;
-
-                    $excellGenerator = new ProductExcellGenerator($sheetName);
-                    $excellGenerator->setSheetHeader($sheetName, $rowHeaderData);
-                    $excellGenerator->addSheetRow($sheetName, $rowData);
-                    $hasUnsavedRows = true;
-                } else {
-                    // Самый первый проход
-                    $excellGenerator = new ProductExcellGenerator($sheetName);
-                    $excellGenerator->setSheetHeader($sheetName, $rowHeaderData);
-                    $hasUnsavedRows = false;
-                }
-            } else {
-                $excellGenerator->addSheetRow($sheetName, $rowData);
-                $hasUnsavedRows = true;
-            }*/
-
             if (++$rowCounter >= self::MAX_ROWS) {
                 $rowCounter = 0;
                 ++$pageCounter;
@@ -117,9 +94,5 @@ class Splitter
             unset($excellGenerator);
             gc_collect_cycles();
         }
-
-        /*if ($excellGenerator && $hasUnsavedRows) {
-            $excellGenerator->saveToFile($this->splittingDirName . '/' . $sheetName . "_{$pageCounter}.xlsx");
-        }*/
     }
 }
